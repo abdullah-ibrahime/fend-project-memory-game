c5022b5844093623684a1816ec2927348ea1929f
abdullah ibrahim <aiaas2010@gmail.com>
2018-05-14 13:36:15 +0300

    creat varebal to catch all element in html

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
-/*
- * Create a list that holds all of your cards
- */
+        //Add vareble to git all element in html
+
+let card = document.getElementsByClassName("card");//git select class card
+let cards = [...card]//select all cards
+let deck = document.querySelector(".deck");
+let star = document.querySelectorAll(".fa-star"); //select stars panel
+let count = document.querySelector(".moves"); //select moves number
+let move = 0;
+let restart = document.querySelector(".restart"); //select restart icon
+let modal = document.getElementById("popup1")//select Congratulations popup
+let matchCard = document.getElementsByClassName("match");//selelct matched cards
+let playAgine=document.getElementById("play-again");//elect play again boton
+   console.log(matchCard);
+
+let opCard = []; //Open card arry 
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -25,14 +29,3 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
